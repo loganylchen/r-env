2fedfa2a4c054f4ce9076e5e9b37e1627bc17f45
Logan <yuelong.chen@link.cuhk.edu.hk>
2023-07-08 22:22:12 +0800

    remove all other packages

--- install_packages_R.R
+++ install_packages_R.R
@@ -1,30 +1,29 @@
 setRepositories(ind = 1:2)
 
 install_packages <- c(
+    "Seurat",
     "circlize",
     "gridExtra",
-    "scatterplot3d",
-    "Seurat",
     "statmod",
     "ggalt",
-    "ggpubr",
     "magick",
     "sctransform",
-    "Rcpp"
+    "Rcpp",
+    "ggpubr"
 )
 
-bioc_packages <- c(
-    "edgeR",
-    "DESeq2",
-    "limma",
-    "PCAtools",
-    "MAST",
-    "org.Hs.eg.db",
-    "clusterProfiler",
-    "dittoSeq",
-    "scater",
-    "glmGamPoi"
-)
+# bioc_packages <- c(
+#     "edgeR",
+#     "DESeq2",
+#     "limma",
+#     "PCAtools",
+#     "MAST",
+#     "org.Hs.eg.db",
+#     "clusterProfiler",
+#     "dittoSeq",
+#     "scater",
+#     "glmGamPoi"
+# )
 
 
 
@@ -35,39 +34,39 @@ for (l in install_packages) {
     }
 }
 
-devtools::install_github("YuLab-SMU/ggtree")
-if (!library(ggtree, logical.return = TRUE)) {
-    quit(status = 1, save = "no")
-}
+# devtools::install_github("YuLab-SMU/ggtree")
+# if (!library(ggtree, logical.return = TRUE)) {
+#     quit(status = 1, save = "no")
+# }
 
-for (l in bioc_packages) {
-    BiocManager::install(l)
-    if (!library(l, character.only = TRUE, logical.return = TRUE)) {
-        quit(status = 1, save = "no")
-    }
-}
+# for (l in bioc_packages) {
+#     BiocManager::install(l)
+#     if (!library(l, character.only = TRUE, logical.return = TRUE)) {
+#         quit(status = 1, save = "no")
+#     }
+# }
 
 
 
-devtools::install_github("jokergoo/ComplexHeatmap")
-if (!library(ComplexHeatmap, logical.return = TRUE)) {
-    quit(status = 1, save = "no")
-}
-devtools::install_github("immunogenomics/presto")
-if (!library(presto, logical.return = TRUE)) {
-    quit(status = 1, save = "no")
-}
+# devtools::install_github("jokergoo/ComplexHeatmap")
+# if (!library(ComplexHeatmap, logical.return = TRUE)) {
+#     quit(status = 1, save = "no")
+# }
+# devtools::install_github("immunogenomics/presto")
+# if (!library(presto, logical.return = TRUE)) {
+#     quit(status = 1, save = "no")
+# }
 
-devtools::install_github("neurorestore/Libra")
-if (!library(Libra, logical.return = TRUE)) {
-    quit(status = 1, save = "no")
-}
+# devtools::install_github("neurorestore/Libra")
+# if (!library(Libra, logical.return = TRUE)) {
+#     quit(status = 1, save = "no")
+# }
 
-}
-devtools::install_github("satijalab/seurat-data")
-if (!library(SeuratData, logical.return = TRUE)) {
-    quit(status = 1, save = "no")
-}
+
+# devtools::install_github("satijalab/seurat-data")
+# if (!library(SeuratData, logical.return = TRUE)) {
+#     quit(status = 1, save = "no")
+# }
 
 
 IRkernel::installspec(name = "VSCODE_DKD_R", displayname = "VSCODE_DKD_R", user = FALSE)
