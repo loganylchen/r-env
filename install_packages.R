328fa667dc93bac2dabdf7f4a44ff72600a4ccd5
Logan <yuelong.chen.btr@gmail.com>
2025-04-01 03:22:03 +0000

    refactor(install_packages.R): replace sys.frame with commandArgs for script dir
    The sys.frame function is deprecated and unreliable for determining the script directory. Using commandArgs is a more robust and standard approach to achieve the same functionality.

--- install_packages.R
+++ install_packages.R
@@ -1,6 +1,8 @@
 setRepositories(ind = 1:3, addURLs = c('https://satijalab.r-universe.dev', 'https://bnprks.r-universe.dev/'))
+args <- commandArgs(trailingOnly = FALSE)
+script_dir <- sub("--file=", "", args[grep("--file=", args)])
 
-script_dir <- dirname(sys.frame(1)$ofile)
+
 base_packages <- scan(file.path(script_dir, "CRAN_packages.txt"), what="", sep="\n")
 bioc_packages <- scan(file.path(script_dir, "bioconductor_packages.txt"), what="", sep="\n")
 github_packages <-  read.table(file.path(script_dir, "devtools_packages.txt"), 
