51b4b9a01adb6655d8a93fd675581116f96845e4
logan <yuelong.chen.btr@gmail.com>
2025-04-01 21:59:04 +0800

    refactor(ci): remove redundant docker run command in workflows
    The `docker run` command to generate the installed packages CSV file was removed as it is now handled directly in the `readme_update.R` script. This change simplifies the workflow and reduces redundancy.

--- readme_update.R
+++ readme_update.R
@@ -1,22 +1,23 @@
 #!/usr/bin/env Rscript
 
+
+
 args <- commandArgs(trailingOnly = TRUE)
+
 
 # 参数验证
 if (length(args) < 2) {
   stop("Usage: Rscript generate_packages_md.R <input.csv> <output.md>", call. = FALSE)
 }
-
-input_file <- args[1]
+packages_csv <- args[1]
 output_file <- args[2]
 
-# 检查文件存在性
-if (!file.exists(input_file)) {
-  stop(paste("Error: Input file", input_file, "not found!"), call. = FALSE)
-}
+pkg_data <-installed.packages()
 
-# 读取CSV文件（自动处理引号）
-pkg_data <- read.csv(input_file, header = TRUE, stringsAsFactors = FALSE)
+
+write.csv(pkg_data[,c('Package', 'Version')], packages_csv, row.names=FALSE,quote=FALSE)
+
+
 
 
 # 修改后的Markdown生成函数
@@ -24,7 +25,7 @@ generate_table_with_citations <- function(pkg_data) {
   # 获取引用信息
   citations <- sapply(pkg_data$Package, function(p) {
     tryCatch({
-      format(citation(p),style='markdown')
+      format(citation(p),style='text')
     }, error = function(e) "Citation not available")
   })
   
